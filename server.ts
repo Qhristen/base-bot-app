9c6016b0644d8b26f9b3257a649b90c7ecbdc9ee
Qhristen <odeyxtian@gmail.com>
2024-04-29 18:16:42 +0100

    change text

--- server.ts
+++ server.ts
@@ -10,18 +10,26 @@ const bot = new Telegraf(`${process.env.TELEGRAM_TOKEN}`);
 
 const web_link = "https://base-bot-app.vercel.app/mobile/tap";
 
-
-
 app.prepare().then(() => {
   const server = express();
   bot.start((ctx) => {
     const username = ctx.message.from.username;
 
-    ctx.reply(`Welcome to Base @${username} added token to env`, {
-      reply_markup: {
-        inline_keyboard: [[{ text: "Play game", web_app: { url: web_link } }]],
-      },
-    });
+    ctx.reply(
+      `
+    Hey, @${username}! Welcome to Base! Tap on the coin and see your balance rise.
+    Base is a Decentralized Exchange on the Solana Blockchain. The biggest part of Base Token TAPS distribution will occur among the players here.
+    Got friends, relatives, co-workers?
+    Bring them all into the game.
+    More buddies, more coins.`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "Play game", web_app: { url: web_link } }],
+          ],
+        },
+      }
+    );
   });
 
   bot.command("referral", async (ctx) => {
@@ -36,11 +44,9 @@ app.prepare().then(() => {
   // Define custom routes or server logic here
 
   server.get("/api", async (req, res) => {
-    const users = await prisma.user.findMany();
     res.status(200).json({
       status: "success",
-      users,
-      message: `Total ${users.length} users`,
+     
     });
   });
   server.get("*", (req, res) => {
