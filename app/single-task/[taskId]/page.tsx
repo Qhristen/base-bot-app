b27bace16e090d091247e9baf7f6ec259d1b23d3
Qhristen <odeyxtian@gmail.com>
2024-06-02 18:36:53 +0100

    remove export

--- app/single-task/[taskId]/page.tsx
+++ app/single-task/[taskId]/page.tsx
@@ -1,6 +1,4 @@
 import TaskDetails from "@/components/mobile/task-details";
-import { SpecialTask } from "@/types";
-import axios from "axios";
 
 interface TaskPageProps {
   params: {
@@ -8,15 +6,15 @@ interface TaskPageProps {
   };
 }
 
-export async function generateStaticParams() {
-  const specialTask = await axios
-    .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/task/special`)
-    .then((res) => res?.data?.data?.task as SpecialTask[]);
+// export async function generateStaticParams() {
+//   const specialTask = await axios
+//     .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/task/special`)
+//     .then((res) => res?.data?.data?.task as SpecialTask[]);
 
-  return specialTask.map((task) => ({
-    taskId: task.id,
-  }));
-}
+//   return specialTask.map((task) => ({
+//     taskId: task.id,
+//   }));
+// }
 
 const page = ({ params }: TaskPageProps) => {
   return (
