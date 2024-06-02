59be7972598059a8858654d9514d4827ed786dca
Qhristen <odeyxtian@gmail.com>
2024-06-02 18:04:30 +0100

    fix dynamic route with generateStaticParams

--- app/single-task/[taskId]/page.tsx
+++ app/single-task/[taskId]/page.tsx
@@ -1,10 +1,23 @@
 import TaskDetails from "@/components/mobile/task-details";
+import { SpecialTask } from "@/types";
+import axios from "axios";
 
 interface TaskPageProps {
   params: {
     taskId: string;
   };
 }
+
+export async function generateStaticParams() {
+  const posts = await axios
+    .get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/task/special`)
+    .then((res) => res?.data?.data?.task as SpecialTask[]);
+
+  return posts.map((post) => ({
+    taskId: post.id,
+  }));
+}
+
 const page = ({ params }: TaskPageProps) => {
   return (
     <>
