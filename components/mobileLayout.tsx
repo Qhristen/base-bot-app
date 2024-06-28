9edd981b3ed8c376a2eb8f7a74e96a9eae3809f2
Qhristen <odeyxtian@gmail.com>
2024-06-28 18:35:30 +0100

    update layout

--- components/mobileLayout.tsx
+++ components/mobileLayout.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useContext } from "react";
 import DesktopMessage from "./desktopMessage";
 import MovileBottomNav from "./mobile/bottom-nav";
+import { isMobile } from "react-device-detect";
+import { TelegramContext } from "@/context/telegram-context";
 
 const MobileLayout = ({ children }: { children: React.ReactNode }) => {
+  const { user } = useContext(TelegramContext);
+
   return (
     <>
-      <div className="hidden lg:block">
+      <div className={`${isMobile ? "hidden" : "block"}`}>
         <DesktopMessage />
       </div>
-      <div className="block lg:hidden">
+      <div className={`${isMobile ? "block" : "hidden"} lg:hidden`}>
         {children}
         <MovileBottomNav />
       </div>
